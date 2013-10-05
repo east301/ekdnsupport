d8b2b8d46cd7eabe414c86084fad0f410631e985
east301 <me@east301.net>
2013-10-05 10:10:32 +0900

    change default time zone and language

--- ekdnsupport/settings.py
+++ ekdnsupport/settings.py
@@ -18,11 +18,11 @@ MANAGERS = ADMINS
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Asia/Tokyo'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja-jp'
 
 SITE_ID = 1
 
