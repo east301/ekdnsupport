ddb893948ec32d9ff59ee34d18e35f1e73b8af8f
east301 <me@east301.net>
2013-10-05 11:36:37 +0900

    add model to store GPX data

--- lib/models.py
+++ lib/models.py
@@ -1,3 +1,30 @@
+import os
+import uuid
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
+from ekdnsupport import settings
 
-# Create your models here.
+
+class GpxData(models.Model):
+    id = models.AutoField(primary_key=True)
+    uid = models.CharField(null=False, unique=True, max_length=32)
+
+    owner = models.ForeignKey(User, null=True)
+    description = models.CharField(null=True, max_length=1024)
+    has_timestamp = models.BooleanField(null=False)
+
+    created = models.DateTimeField(null=False)
+
+    def save(self, *args, **kwargs):
+        if self.id is None:
+            if self.uid is None:
+                self.uid = uuid.uuid4().hex
+            if self.created is None:
+                self.created = timezone.now()
+
+        return super(GpxData, self).save(*args, **kwargs)
+
+    @property
+    def path(self):
+        return os.path.join(settings.GPX_ROOT, self.uid + '.gpx')
