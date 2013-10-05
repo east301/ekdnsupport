1d069f355cd7a35e6fb97380708cd5fb116425ff
east301 <me@east301.net>
2013-10-05 10:21:46 +0900

    set path to SQLite database file

--- ekdnsupport/settings_development.py
+++ ekdnsupport/settings_development.py
@@ -1,18 +1,12 @@
 # Django settings for development environment.
 
+import dj_database_url
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
